a15039b36911851c1901ebe78bdd1453f8bd1dba
EighteenGray <EighteenGray@163.com>
2017-05-09 16:46:04 +0800

    修改cutactivity

--- app/src/main/java/com/eighteengray/procamera/activity/CutActivity.java
+++ app/src/main/java/com/eighteengray/procamera/activity/CutActivity.java
@@ -66,7 +66,7 @@ public class CutActivity extends BaseActivity
         setContentView(R.layout.aty_cut);
         ButterKnife.bind(this);
 
-        width = 600;
+        width = ScreenUtils.getScreenWidth(CutActivity.this);
         path = getIntent().getStringExtra(Constants.CROPIMAGEPATH);
         bitmap = ImageUtils.getBitmapFromPath(path);
         drawable = DataConvertUtil.bitmap2Drawable(bitmap);
