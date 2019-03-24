921fda86ba76b6dc281dfb74227c9febc90fb7dd
lutao.23 <lutao.23@bytedance.com>
2019-03-25 01:14:29 +0800

    [update]引入opencv_cpp

--- app/src/main/java/com/eighteengray/procamera/imageprocess/activity/FilterActivity.java
+++ app/src/main/java/com/eighteengray/procamera/imageprocess/activity/FilterActivity.java
@@ -4,9 +4,13 @@ package com.eighteengray.procamera.imageprocess.activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
+
 import com.eighteengray.imageprocesslibrary.ImageProcessBaseActivity;
 import com.eighteengray.imageprocesslibrary.ImageProcessByOpenCv;
+import com.eighteengray.imageprocesslibrary.ImageProcessJni;
 import com.eighteengray.procamera.R;
+
+import java.io.ByteArrayOutputStream;
 
 
 public class FilterActivity extends ImageProcessBaseActivity
@@ -27,6 +31,16 @@ public class FilterActivity extends ImageProcessBaseActivity
         imageView = (ImageView) findViewById(com.eighteengray.imageprocesslibrary.R.id.image_view);
         Bitmap bitmap = ImageProcessByOpenCv.bitmap2Mat();
         imageView.setImageBitmap(bitmap);
+        /*int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+        int[] pixels = new int[w*h];
+        bitmap.getPixels(pixels, 0, w, 0, 0, w, h);
+
+        int[] resultInt = ImageProcessJni.toGray(pixels, w, h);
+        Bitmap resultImg = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        resultImg.setPixels(resultInt, 0, w, 0, 0, w, h);
+        imageView.setImageBitmap(resultImg);*/
+
     }
 
 
